cc599bd790062e866da1d80d5a725bafaef69960
jungchanSon <rnrmfjc@gmail.com>
2025-03-20 19:18:28 +0900

    feat: update download link

--- features/Landing/Download.tsx
+++ features/Landing/Download.tsx
@@ -9,7 +9,7 @@ const Download = () => {
             <div className={"bg-green-50 p-10"}>
                 <ContentTitle> 다운로드 </ContentTitle>
                 <a className={"rounded-md p-2 bg-cyan-500 shadow-lg shadow-cyan-500/50"}
-                   href={"https://drive.usercontent.google.com/download?id=13nybhPIKGdH_0HDd6waFBVcub8nsx29Y&export=download&authuser=0"}
+                   href={"https://drive.google.com/file/d/1FtEulRx0WHGwkwzaFsIOO3HjQ_J6UDaU/view?usp=drive_link"}
                 >
                     다운로드
                 </a>
